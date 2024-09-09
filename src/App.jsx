fac4e3d71d6c7866aad48b20acaa6f71058ac711
t-t13 <thanapat@sirisoft.co.th>
2024-09-09 11:17:21 +0700

    react refs & portal # 01 : basic 101

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import GameBoard from "./GameBoard/GameBoard"
-import Player from "./Player/player"
-import { useState } from "react"
-
+import Player from './components/Player.jsx';
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
-
-  const handleSelectMarkPoint = () => {
-    setActivePlayer((curActivevPlayer) => curActivevPlayer === 'X' ? 'O' : 'X' 
-    )
-  }
-
   return (
-    <main>
-      <div id="game-container">
-        <ol id="players" className="highlight-player">
-          <Player playerName="player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player playerName="player 2" symbol="O" isActive={activePlayer === 'O'} />
-        </ol>
-        <GameBoard changeActivePlayer={handleSelectMarkPoint} activeSymbol={activePlayer} />
-      </div>
-    </main>
-  )
+    <>
+      <Player />
+      <div id="challenges"></div>
+    </>
+  );
 }
 
-export default App
+export default App;
