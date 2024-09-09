024c532a4ad1fdbaa615880f218d55976e004c2a
t-t13 <thanapat@sirisoft.co.th>
2024-09-09 15:55:33 +0700

    react refs & portal # 03 : player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [currentPlayerName, setCurrentPlayerName] = useState();
+  const playerName = useRef();
+
+  const handleChangeName = () => {
+    setCurrentPlayerName(playerName.current.value);
+    // playerName.current.value = ''
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {currentPlayerName ?? 'unknown player.'} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleChangeName}>Set Name</button>
       </p>
     </section>
   );
