6b42dd62ff4111aafaca8b753cbad7e0113ec039
t-t13 <thanapat@sirisoft.co.th>
2024-09-09 19:15:21 +0700

    react refs & portal # 04 : player

--- src/GameBoard/GameBoard.jsx
+++ src/GameBoard/GameBoard.jsx
@@ -6,12 +6,38 @@ const initialGameBoard = [
     [null, null, null]
 ]
 
-export default function GameBoard({changeActivePlayer,activeSymbol}) {
+function a (){
+    console.log("hello world");
+    
+}
+
+const b = (text)=>{
+    console.log(text);
+    
+}
+
+export default function GameBoard({ changeActivePlayer, activeSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     const handleSelectMarkPoint = (rowIndex, colIndex) => {
+        // console.log("tests");
+        
+        // console.log(initialGameBoard);
+        
+        // let arr_a = initialGameBoard; 
+        // let arr_b = {...initialGameBoard};
+
+        // console.log(arr_a);
+        // console.log(arr_b);
+        
+        // arr_b[3][0] = "B"
+        // console.log(arr_a);
+        // console.log(arr_b);
+        // console.log(initialGameBoard);
+    
         setGameBoard((prevGameBoard) => {
-            const updateGameBoard = [...prevGameBoard.map(inPrev => [...inPrev])]
+            const updateGameBoard = [...prevGameBoard.map((inPrev) =>
+                [...inPrev])]
             updateGameBoard[rowIndex][colIndex] = activeSymbol;
             return updateGameBoard;
         });
