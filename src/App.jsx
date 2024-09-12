7ca3e022a203f1a5f6ea3e36ec99372c4cf192d9
t-t13 <thanapat@sirisoft.co.th>
2024-09-12 09:42:37 +0700

    react hook # 02 : the-all-example-hook-in-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useRef, useState } from 'react';
-
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import { useCallback, useEffect, useRef, useState } from "react";
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [showText, setShowText] = useState();
+
+  useEffect(() => {
+    const text = "this is infinnite loop.";
+    setShowText(text);
+
+    const storeBG = JSON.parse(localStorage.getItem("backgroundPlaces")) || [];
+    const listBGs = storeBG.map((sId) => {
+      return AVAILABLE_PLACES.find((place) => place.id === sId);
+    });
+
+    setPickedPlaces(listBGs);
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,14 +40,36 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storeBG = JSON.parse(localStorage.getItem("backgroundPlaces")) || [];
+    if (storeBG.indexOf(id) === -1) {
+      localStorage.setItem(
+        "backgroundPlaces",
+        JSON.stringify([...storeBG, id])
+      );
+    }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(
+    function handleRemovePlace() {
+    console.log("before re-ex");
+    
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
+
+    console.log("after re-ex");
+
     modal.current.close();
-  }
+
+    console.log("after re-ex-2");
+
+    const storeBG = JSON.parse(localStorage.getItem("backgroundPlaces")) || [];
+    localStorage.setItem(
+      "backgroundPlaces",
+      JSON.stringify(storeBG.filter((id) => id !== selectedPlace.current))
+    );
+  }, []);
 
   return (
     <>
@@ -57,7 +91,7 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
