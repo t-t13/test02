d50a5512fc461451b7762460048531144ab96c56
t-t13 <thanapat@sirisoft.co.th>
2024-09-13 17:40:42 +0700

    react router #02 : basic router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./Page/Home/HomePage";
+import JumpPage from "./Page/Jump/JumpPage";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/jump", element: <JumpPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
